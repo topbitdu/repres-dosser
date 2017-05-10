a8f76924135e7eec8f459714ec6e74ea82c8ddaf
Topbit Du <topbit.du@gmail.com>
2017-05-10 09:51:30 +0800

    1, Improve the Resource Presentation concern for the document.

--- app/controllers/repres/dosser/concerns/resource_presentation.rb
+++ app/controllers/repres/dosser/concerns/resource_presentation.rb
@@ -241,7 +241,14 @@ module Repres::Dosser::Concerns::ResourcePresentation
     end
 
     ##
-    # 按照给定的参数响应结果。此为内部方法。
+    # 按照给定的参数响应结果。此为内部方法。如：
+    # respond_result :ok,
+    #   success:    true,
+    #   code:       'success',
+    #   message:    '成功',
+    #   collection: [ { id: 'id-1', name: 'name-1' }, { id: 'id-2', name: 'name-2' } ],
+    #   size:       2,
+    #   errors:     {}
     def respond_result(status, result)
       result[:meta] = { criteria: @criteria, request_id: request.uuid, timestamp: Time.now.to_i }
       result[:meta][:pagination] = @pagination if @pagination.present?
